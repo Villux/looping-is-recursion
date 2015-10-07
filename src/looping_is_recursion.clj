85e84c8e29040d16fcab8ec3629135a3b4e018ae
Ville Toiviainen <villej.toiviainen@gmail.com>
2015-10-07 23:05:52 +0300

    partial implementation of exercise

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,19 +1,46 @@
 (ns looping-is-recursion)
 
 (defn power [base exp]
-  ":(")
+  (let [ helper (fn
+                  [acc base exp]
+                  (cond
+                   (zero? exp) 1
+                   (= exp 1) acc
+                   :else (recur (* acc base) base (dec exp))))
+         ]
+    (helper base base exp)))
+
+(first '())
 
 (defn last-element [a-seq]
-  ":(")
+  (if (<= (count a-seq) 1)
+    (first a-seq)
+    (recur (rest a-seq))))
+
 
 (defn seq= [seq1 seq2]
-  ":(")
+  (cond
+   (and (empty? seq1) (empty? seq2)) true
+   (or (empty? seq1) (empty? seq2)) false
+   (= (first seq1) (first seq2)) (recur (rest seq1) (rest seq2))
+   :else false))
 
 (defn find-first-index [pred a-seq]
-  ":(")
+  (loop [i 0
+         loop-seq a-seq]
+    (cond
+     (pred (first loop-seq)) i
+     (empty? (rest loop-seq)) nil
+     :else (recur (inc i) (rest loop-seq)))))
 
 (defn avg [a-seq]
-  -1)
+  (loop [acc 0
+         i 0
+         loop-seq a-seq]
+    (if (empty? loop-seq)
+      (/ acc i)
+      (recur (+ acc (first loop-seq)) (inc i) (rest loop-seq)))))
+
 
 (defn parity [a-seq]
   ":(")
